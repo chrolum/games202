6f3bed4c76e79908cc956bca6fbf72e391c12c82
saltsuica <crkylin@gmail.com>
2021-05-28 17:09:00 +0800

    use prt data to calculate color shader

--- homework2/src/shaders/PRTShader/PRTFragmentShader.glsl
+++ homework2/src/shaders/PRTShader/PRTFragmentShader.glsl
@@ -2,7 +2,9 @@
 precision mediump float;
 #endif
 
+varying highp vec3 vColor;
 
 void main(void)
 {
-}
+    gl_FragColor = vec4(vColor, 1.0);
+}   
--- homework2/src/shaders/PRTShader/PRTVertexShader.glsl
+++ homework2/src/shaders/PRTShader/PRTVertexShader.glsl
@@ -7,6 +7,7 @@ attribute vec3 aVertexPosition;
 attribute vec3 aNormalPosition;
 attribute vec2 aTextureCoord;
 
+uniform vec3 uPrecomputeL[9];
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uProjectionMatrix;
@@ -14,13 +15,42 @@ uniform mat4 uProjectionMatrix;
 varying highp vec2 vTextureCoord;
 varying highp vec3 vFragPos;
 varying highp vec3 vNormal;
-varying highp vec4 vColor;
+varying vec3 vColor;
 
 void main(void) {
 
   vFragPos = (uModelMatrix * vec4(aVertexPosition, 1.0)).xyz;
   vNormal = (uModelMatrix * vec4(aNormalPosition, 0.0)).xyz;
-  vColor = aPrecomputeLT
+
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      vColor[0] += uPrecomputeL[i][0] * aPrecomputeLT[0][j];
+      vColor[1] += uPrecomputeL[i][1] * aPrecomputeLT[0][j];
+      vColor[2] += uPrecomputeL[i][2] * aPrecomputeLT[0][j];
+    }
+  }
+
+  for (int i = 3; i < 6; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      vColor[0] += uPrecomputeL[i][0] * aPrecomputeLT[1][j];
+      vColor[1] += uPrecomputeL[i][1] * aPrecomputeLT[1][j];
+      vColor[2] += uPrecomputeL[i][2] * aPrecomputeLT[1][j];
+    }
+  }
+
+  for (int i = 6; i < 9; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      vColor[0] += uPrecomputeL[i][0] * aPrecomputeLT[2][j];
+      vColor[1] += uPrecomputeL[i][1] * aPrecomputeLT[2][j];
+      vColor[2] += uPrecomputeL[i][2] * aPrecomputeLT[2][j];
+    }
+  }
 
   gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix *
                 vec4(aVertexPosition, 1.0);
